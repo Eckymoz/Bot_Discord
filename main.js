5ac75cc76ff40ac4ad32a7c41cef638debb1daf6
Grégory <nop>
2020-04-12 19:28:22 +0200

    Add role command + fix bugs

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-const {PREFIX, TOKEN} = require ("./config");
-const {Client} = require('discord.js');
-const client = new Client({disableEveryone: true});
+const { PREFIX, TOKEN } = require('./config');
+const { Client } = require('discord.js');
+const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
-  console.log(`Je suis prêt ! `);
+client.on('message', (msg) => {
+  if (msg.author.bot) return;
+  const args = msg.content.split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  const role = msg.guild.roles.cache.find((r) => r.name === args[0]);
+  if (cmd === `${PREFIX}ping`) msg.reply('Pong!');
+  if (cmd === `${PREFIX}repeat`) {
+    msg.channel.send(args.join(' '));
+    msg
+      .delete({ timeout: 3000 })
+      .then((msg) => console.log('Un message a été supprimé'));
+  }
+  if (cmd === `${PREFIX}role`) {
+    if (!args[0])
+      return msg.reply(
+        'Il faut que tu spécifie le role tu a le choix entre PC PS4 Switch XBOX et Mobile'
+      );
+    if (!role) return msg.channel.send("Ce rôle n'existe pas !");
+    if (msg.member.roles.cache.find((r) => r.name === args[0])) {
+      msg.member.roles.remove(role);
+      msg.channel.send(`J'ai supprimé le rôle ${role} à ${msg.author}.`);
+    } else {
+      msg.member.roles.add(role);
+      msg.channel.send(`J'ai ajouté le rôle ${role} à ${msg.author}.`);
+    }
+  }
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(`${PREFIX}ping`))  msg.reply('Pong!');
+client.on('guildMemberAdd', (member) => {
+  member.send('Salut à toi !');
+  const channel = client.channels.cache.find((r) => r.name === 'general');
+  channel.send(`${member} a rejoint le serveur !`);
 });
 
 client.login(TOKEN);
+
+client.on('ready', () => console.log('Je suis prêt'));
+client.on('error', console.error);
+client.on('warn', console.warn);
+client.on('debug', console.debug);

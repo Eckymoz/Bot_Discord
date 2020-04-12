948528d5a247b03d95ef78537eaeb1cfed4507e0
Grégory <nop>
2020-04-12 14:44:13 +0200

    Add configuration file

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
+const {PREFIX, TOKEN} = require ("./config");
 const {Client} = require('discord.js');
 const client = new Client({disableEveryone: true});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Je suis prêt ! `);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping')  msg.reply('Pong!');
-  if (msg.content === 'everyone')  msg.reply('@everyone Salut à tous !');
-  if (msg.content === 'noteeveryone')  msg.reply('@everyone (noteeveryone) Salut à tous !');
-  
+  if (msg.content.startsWith(`${PREFIX}ping`))  msg.reply('Pong!');
 });
 
-client.login('Njk4ODM4NTMyMDg4NzkxMTAx.XpMAAg.3FEND7O1CIrCKIN5MCcFXvYYcVM');
+client.login(TOKEN);
